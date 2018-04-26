2c9a7920d6cf778e9a383379ca8a1fe48a8a150d
Dylan Nissley <dylan.nissley@gmail.com>
2018-04-26 09:06:49 -0500

    Switch to jetty in servlets example and get it working

--- servlets/src/main/java/com/dnissley/servlets/Application.java
+++ servlets/src/main/java/com/dnissley/servlets/Application.java
@@ -1,32 +1,15 @@
 package com.dnissley.servlets;
 
-import javax.servlet.ServletException;
-
-import io.undertow.Undertow;
-import io.undertow.server.HttpHandler;
-import io.undertow.servlet.Servlets;
-import io.undertow.servlet.api.DeploymentInfo;
-import io.undertow.servlet.api.DeploymentManager;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHandler;
 
 public class Application {
-  public static void main(String[] args) throws ServletException {
-    DeploymentInfo servletBuilder = Servlets.deployment()
-      .setClassLoader(Application.class.getClassLoader())
-      .setContextPath("/app")
-      .setDeploymentName("test.war")
-      .addServlet(Servlets.servlet("HelloServlet", HelloServlet.class).addMapping("/hello"));
-
-    DeploymentManager manager = Servlets.defaultContainer().addDeployment(servletBuilder);
-    manager.deploy();
-
-    HttpHandler servletHandler = manager.start();
-
-    Undertow server = Undertow.builder()
-      .addHttpListener(8080, "localhost")
-      .setHandler(servletHandler)
-      .build();
-
+  public static void main(String[] args) throws Exception {
+    Server server = new Server(8080);
+    ServletHandler handler = new ServletHandler();
+    server.setHandler(handler);
+    handler.addServletWithMapping(HelloServlet.class, "/*");
     server.start();
+    server.join();
   }
 }
-
